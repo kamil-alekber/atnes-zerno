a595ab60038323e60275c14e849232224c160f27
kalekberov <kamil.alekberov@ps.kz>
2020-03-24 11:23:31 +0600

    feat: add env files

--- next.config.js
+++ next.config.js
@@ -2,10 +2,19 @@ const withSass = require("@zeit/next-sass");
 const withCSS = require("@zeit/next-css");
 const withImages = require("next-images");
 
+require("dotenv").config(
+  process.env.APP_TYPE === "dev"
+    ? {
+        path: "./dev.env"
+      }
+    : { path: "./prod.env" }
+);
+
 module.exports = withImages(
   withSass(
     withCSS({
       esModule: false,
+      distDir: process.env.NEXT_OUT_DIR,
       webpack: function(config) {
         config.module.rules.push({
           test: /\.(eot|woff|woff2|ttf|jpeg|png|svg)$/,
